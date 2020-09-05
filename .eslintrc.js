06dc44fe400fe834f394c99ae115b096875de856
ng315429 <ng315429@gmail.com>
2020-09-05 16:59:51 +0900

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,17 +9,20 @@ module.exports = {
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  },
-  overrides: [
-    {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ]
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-unused-var":process.env.NODE_ENV === "production" ? "warn" : "off",
+    'prettier/prettier': [
+      'error',
+      {
+          singleQuote: true,
+          semi: true,
+          useTabs: false,
+          tabWidth: 2,
+          trailingComma: 'all',
+          printWidth: 80,
+          bracketSpacing: true,
+          arrowParens: 'avoid',          
+      },
+    ],
+  }
 };
---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  devServer: {
+    overlay: false,
+  },
+  // css: {
+  //   loaderOptions: {
+  //     sass: {
+  //       prependData: `@import "@/assets/scss/main.scss";`,
+  //     },
+  //   },
+  // },
+};
